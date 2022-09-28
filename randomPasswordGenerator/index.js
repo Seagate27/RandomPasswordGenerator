1799a0da1e730d1d18e00efda070bddeda437396
Seagate27 <114462449+Seagate27@users.noreply.github.com>
2022-09-28 10:39:56 +0800

    Finalize css

--- randomPasswordGenerator/index.js
+++ randomPasswordGenerator/index.js
@@ -1,17 +1,20 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
 
-let generateButton = document.querySelector("#generateButton")
-let randomPassOne = document.querySelector("#randomPassOne")
-let randomPassTwo = document.querySelector("#randomPassTwo")
+let generateButton = document.getElementById('generateButton')
+let randomPassOne = document.getElementById('randomPassOne')
+let randomPassTwo = document.getElementById('randomPassTwo')
 
-generateButton.addEventListener("click", function(){
+generateButton.addEventListener("click", function () {
+    let passOne = '';
+    let passTwo = '';
     for (let i = 0; i < 15; i++) {
-        let randomOne = Math.floor(Math.random() * characters.length) 
+        let randomOne = Math.floor(Math.random() * characters.length)
         let randomTwo = Math.floor(Math.random() * characters.length)
-        passOne = characters[randomOne]
-        passtwo = characters[randomTwo]
-        randomPassOne.textContent += passOne
-        randomPassTwo.textContent += passtwo
+        passOne += characters[randomOne]
+        passTwo += characters[randomTwo]
+
     }
+    randomPassOne.textContent = passOne
+    randomPassTwo.textContent = passTwo
 })
